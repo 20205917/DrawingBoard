45c3e97a5433f1e89c615323bd29da6830e845bb
yujie <2025575181@qq.com>
2022-10-23 19:58:44 +0800

    测试开始

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 1;
+        JFrame x = new JFrame();
+        x.getLayeredPane().add;
+        x.getGlassPane();
+        x.getContentPane();
     }
 }
