77288da0dcb095a1ae9e5d2c5717969b86921cc9
yujie <2025575181@qq.com>
2022-11-03 19:53:54 +0800

    分工1

--- src/JPanels/Jline/JLine.java
+++ src/JPanels/Jline/JLine.java
@@ -25,16 +25,5 @@ class Point{
         px = x;
         py = y;
     };
-    public int getPx() {
-        return px;
-    }
-    public void setPx(int px) {
-        this.px = px;
-    }
-    public int getPy() {
-        return py;
-    }
-    public void setPy(int py) {
-        this.py = py;
-    }
+
 }
