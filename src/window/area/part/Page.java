cabea070af99bc60449d224ed44927a6587cda8b
C-Al-V-In <864665308@qq.com>
2022-11-11 15:41:56 +0800

    Page

--- src/window/area/part/Page.java
+++ src/window/area/part/Page.java
@@ -1,11 +1,14 @@
 package window.area.part;
 
 import javax.swing.*;
+import java.awt.image.BufferedImage;
 
 //小窗口包含白板和按钮
 public class Page extends JButton {
     public static  double AspectrRatio = 3/4.0;
     public Board board;
+    // 缩略图
+    public BufferedImage thumbnail;
     public Page(Board B){
         board = B;
 //        jb.setIconTextGap(0);//将标签中显示的文本和图标之间的间隔量设置为0
